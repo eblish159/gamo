dcbd07bb0d20813767c48cac4902bf3bf7e0ab91
eblish159 <tkdgud159@naver.com>
2024-12-06 16:55:27 +0900

    Add files via upload

---
+++ gamo/src/main/java/fs/four/gamo/GamoApplication.java
@@ -0,0 +1,13 @@
+package fs.four.gamo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamoApplication.class, args);
+    }
+
+}
---
+++ gamo/src/test/java/fs/four/gamo/GamoApplicationTests.java
@@ -0,0 +1,13 @@
+package fs.four.gamo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
