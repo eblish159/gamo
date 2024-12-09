b36bacac59f3b5c3a0b63e5192b64bcf7fe2b8b8
codefig7 <codefig7@gmail.com>
2024-12-09 09:04:04 +0900

    mybatis issue

--- src/main/java/fs/four/gamo/member/controller/LoginController.java
+++ src/main/java/fs/four/gamo/member/controller/LoginController.java
@@ -1,7 +1,5 @@
 package fs.four.gamo.member.controller;
 
-import fs.four.gamo.member.service.LoginService;
-import fs.four.gamo.member.vo.LoginVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,25 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
-    @Autowired
-    private LoginService loginService;
 
     @RequestMapping("/login")
     public String loginPage() {
         return "test";
     }
 
-    @PostMapping("/login")
-    public String login(LoginVO loginVO, Model model) {
-        LoginVO loginCheck = loginService.login(loginVO);
-        if (loginCheck != null) {
-            model.addAttribute("user", loginCheck);
-            return "member/result";
-        } else {
-            model.addAttribute("error", "로그인 실패");
-            return "member/test1";
-        }
-    }
 }
 
 
